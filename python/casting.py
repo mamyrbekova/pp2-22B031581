cfdb3ba23fc04d7fe5d91caa5da165ab1d10278d
mamyrbekova <122817142+mamyrbekova@users.noreply.github.com>
2023-01-20 19:38:30 +0600

    Delete python directory

--- python/casting.py
+++
@@ -1,7 +0,0 @@
-x = str("hello")
-y = int(8)
-z = float(4)
-
-print(x)
-print(y)
-print(z)
--- python/string2.py
+++
@@ -1,8 +0,0 @@
-a = " Types of programming "
-
-if "of" in a:
-    print("Yes")
-
-print(a.upper())    # turn into uppercase
-print(a.lower())    # turn into lowercase
-print(a.strip())    # delete the whit   espaces in the beginning and at the end
