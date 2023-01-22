06d339a9fd9f306dadde5da46eba376cc7a6a80c
mamyrbekova <122817142+mamyrbekova@users.noreply.github.com>
2023-01-22 21:07:24 +0600

    Update README.md

---
+++ lab2/list_method.py
@@ -0,0 +1,12 @@
+animals = ["panda", "dog", "bear", "kitten"]
+numbers = [10, 20, 30, 10, 40, 10, 50, 20]
+# count/sorted/min/max
+print(numbers.count(10))   # count specific numbers in list
+
+x = sorted(animals)
+print(x)
+
+print(min(numbers))
+print(max(numbers))
+
+
